c208b3e4f619eaa0ab576c1ac80dd2cd3a3c88e0
Makhambet <mahakurotsuchi@gmail.com>
2023-06-30 00:48:25 +0600

    Added ChatGPT API to telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,32 @@
 import telebot
 import webbrowser
+import json
+import openai
+
+with open('key.json') as f:
+    data = json.load(f)
+key = data['key']
+
+openai.api_key = key
 
 bot = telebot.TeleBot('6341190720:AAEmekg_0fO1OrQVplWEIJfSAsra6g9BHwU')
+
+
+def generate_response(text):
+    response = openai.Completion.create(
+        prompt=text,
+        engine='text-davinci-003', #модель
+        max_tokens=100, #количество токенов в ответе
+        temperature=0.7, #отвечает за креативность желаемого ответа от 0 до 1 (0-очень прямой ответ, 1 - очень креативный)
+        n=1, #количество ответов
+        stop=None, #указывает каким словом должен заканчиваться ответ
+        timeout=20
+    )
+
+    if response.choices[0].text:
+        return response.choices[0].text.strip() # 0 это номер ответа в списке ответов
+    else:
+        return None
 
 
 @bot.message_handler(commands=['start', 'main', 'hello'])
@@ -25,6 +50,9 @@ def info(message):
         bot.send_message(message.chat.id, f'Yo, {message.from_user.first_name}!')
     elif message.text.lower() == 'id':
         bot.reply_to(message, f'ID: {message.from_user.id}')
+    else:
+        response = generate_response(message.text)
+        bot.send_message(message.chat.id, f'{response}')
 
 
 bot.polling(non_stop=True)
